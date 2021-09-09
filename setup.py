3386378ca636f103014b3113022b186bd606ecb3
tuvovan <vovantu.hust@gmail.com>
2021-09-09 13:06:08 +0900

    update grpc package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=["opencv-python", 
                         "tqdm",
                         "numpy",
-                        "grpc",
+                        "grpcio",
                         "kazoo",
                         "bs4",
                         "pandas",
